f1e59b36e931efeb02a875c13a7589d25b48994b
PARKMINJU <dbwk0404@hanmail.com>
2020-08-26 23:05:19 +0900

    #2.11 MVC controller

---
+++ controllers/userController.js
@@ -0,0 +1,3 @@
+export const join =(res,req) =>res.send("Join");
+export const login =(res,req) =>res.send("Loin");
+export const logout =(res,req) =>res.send("Logout");
---
+++ controllers/videoController.js
@@ -0,0 +1,2 @@
+export const home=(req, res) => res.send("HOME"); //home controller
+export const search = (req, res) => res.send("SERACH");
