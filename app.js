02578dffea73474fa296d045b7c66d7e149b3580
PARKMINJU <dbwk0404@hanmail.com>
2020-08-27 16:55:58 +0900

    #2.13 install Pug

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ import globalRouter from "./routers/globalRouter";
 import routes from "./routes"
 const app = express(); //app변수에 express를 실행
 
+//middleware
+app.set("view engine","pug") //view 엔진을 pug 확장자로 정해준다.
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true})); //form
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,2 +1,7 @@
-export const home=(req, res) => res.send("HOME"); //home controller
+export const home=(req, res) => res.render("home"); //home controller
 export const search = (req, res) => res.send("SERACH");
+export const videos =(reqm ,res) =>res.send("Videos");
+export const upload =(reqm ,res) =>res.send("Upload");
+export const videoDetail =(reqm ,res) =>res.send("Video Detail");
+export const editVideo =(reqm ,res) =>res.send("Edit Video");
+export const deleteVideo =(reqm ,res) =>res.send("Delete Video");
