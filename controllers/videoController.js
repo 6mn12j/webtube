65791ed0c38959ddbfb4759ba8d6f82f4edd5b58
PARKMINJU <dbwk0404@hanmail.com>
2020-08-27 17:17:29 +0900

    #2.14 Layouts with Pug and add pugs files

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,7 @@
 export const home=(req, res) => res.render("home"); //home controller
-export const search = (req, res) => res.send("SERACH");
-export const videos =(reqm ,res) =>res.send("Videos");
-export const upload =(reqm ,res) =>res.send("Upload");
-export const videoDetail =(reqm ,res) =>res.send("Video Detail");
-export const editVideo =(reqm ,res) =>res.send("Edit Video");
-export const deleteVideo =(reqm ,res) =>res.send("Delete Video");
+export const search = (req, res) => res.render("SERACH");
+export const videos =(reqm ,res) =>res.render("Videos");
+export const upload =(reqm ,res) =>res.render("Upload");
+export const videoDetail =(reqm ,res) =>res.render("Video Detail");
+export const editVideo =(reqm ,res) =>res.render("Edit Video");
+export const deleteVideo =(reqm ,res) =>res.render("Delete Video");
