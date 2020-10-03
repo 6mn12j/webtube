2c14e93e93a790bc820ad4fcc172fad1e432be75
PARKMINJU <dbwk0404@hanmail.com>
2020-10-03 22:11:04 +0900

    #5 STYLING

--- middlewares.js
+++ middlewares.js
@@ -1,17 +1,17 @@
 import multer from "multer";
 import routes from "./routes";
 
-const multerVideo = multer({dest: "uploads/videos/"});
+const multerVideo = multer({ dest: "uploads/videos/" });
 
-export const localsMiddleware = (req, res, next) =>{
-    res.locals.siteName ="WeTube";
-    res.locals.routes = routes;
-    //fake login info
-    res.locals.user = {
-        isAuthenticated: true,
-        id: 1
-    };
-    next()
+export const localsMiddleware = (req, res, next) => {
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  //fake login info
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1,
+  };
+  next();
 };
 
 //locals에 있는 건 템플릿에서 변수명 처럼 존재
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require("path");
+
 const autoprefixer = require("autoprefixer");
 const ExtractCSS = require("extract-text-webpack-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
-const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js"); 
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ["@babel/polyfill", ENTRY_FILE],
+  entry: ["@babel/polyfill", ENTRY_FILE], 
   mode: MODE,
   module: {
     rules: [
