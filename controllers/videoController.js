2296e76dcd993e02dcbe1cf1e9f53d187e3a6414
6mn12j <dbwk0404@hanmail.net>
2020-10-19 23:52:17 +0900

    #10.2 API Adding postComment

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,5 +1,6 @@
 import routes from "../routes";
 import Video from "../models/Video";
+import Comment from"../models/Comment";
 
 export const home = async (req, res) => {
   try {
@@ -52,7 +53,9 @@ export const videoDetail = async (req, res) => {
     params: { id }
   } = req;
   try {
-    const video = await Video.findById(id).populate("creator");
+    const video = await Video.findById(id)
+    .populate("creator")
+    .populate("comments");
     res.render("videoDetail", { pageTitle: video.title, video });
   } catch (error) {
     res.redirect(routes.home);
@@ -123,5 +126,29 @@ export const postRegisterView = async(req, res) =>{
     res.end();
   } finally{
     res.end();
+  }
+}
+
+//Add Comment
+
+export const postAddComment = async(req,res)=>{
+  const{
+    params:{id},
+    body:{comment},
+    user
+  }= req;
+  try{
+    const video = await Video.findById(id);
+    const newComment = await Comment.create({
+      text:comment, // body에서옴 사용자가 입력하는 값
+      creator:user.id
+    });
+    video.comments.push(newComment.id);
+    video.save();
+  } catch(error){
+    res.status(400);
+  }
+  finally{
+    res.end()
   }
 }
--- routes.js
+++ routes.js
@@ -30,6 +30,7 @@ const GITHUB_CALLBACK = "/auth/github/callback";
 
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
 
 const routes = {
   home: HOME,
@@ -73,8 +74,9 @@ const routes = {
   gitHub: GITHUB,
   githubCallback: GITHUB_CALLBACK,
   me: ME,
-    api: API,
-  registerView: REGISTER_VIEW
+  api: API,
+  registerView: REGISTER_VIEW,
+  addComment:ADD_COMMENT
   };
 
 export default routes;
