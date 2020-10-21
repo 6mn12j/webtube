9dd42199b299f141468ece270f4b3b0caf6d531a
6mn12j <dbwk0404@hanmail.net>
2020-10-22 01:10:25 +0900

    10.4 Adding Comment complete

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 const addCommentForm = document.getElementById("jsAddComment");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
 
 const sendComment = async (comment) =>{
     const videoId = window.location.href.split("/videos/")[1];
@@ -11,6 +13,24 @@ const sendComment = async (comment) =>{
         }
     });
     console.log(response);
+    if(response.status === 200){
+    addComment(comment);
+    }
+}
+
+//fake 
+
+const increaseNumber=() =>{
+    commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+}
+const addComment = (comment) =>{
+    const li = document.createElement("li");
+    const span =document.createElement("span");
+    span.innerHTML =comment;
+    li.appendChild(span);
+    commentList.prepend(li);
+    increaseNumber();
+
 }
 
 
