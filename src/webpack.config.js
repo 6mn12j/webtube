213f78e669c78cd71660d8d280bbe3fe36ba4337
6mn12j <dbwk0404@hanmail.net>
2020-10-28 00:11:36 +0900

    heroku error ㅠㅠ

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 const autoprefixer = require("autoprefixer");
-const ExtractCSS = require("extract-text-webpack-plugin");
+const  MiniCssExtractPlugin  =  require ( 'mini-css-extract-plugin' ) ;
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js"); 
@@ -22,33 +22,31 @@ const config = {
       },
       {
         test: /\.(scss)$/, //정규 표현식scss인 파일 다 찾음
-        use: ExtractCSS.extract([
+        use:[
+          MiniCssExtractPlugin.loader,
           {
             loader: "css-loader",
           },
           {
             loader: "postcss-loader",
             options: {
-              postcssOptions: {
                 plugins() {
                   return [autoprefixer({ browsers: "cover 99.5%" })];
                 },
-              },
             },
           },
           {
             loader: "sass-loader",
           },
-        ]),
+        ],
       },
     ], //wrbpaack아 scss인 파일을 만날때 마다 어떤 loader를 실행 하거라
   },
   output: {
     path: OUTPUT_DIR,
     filename: "[name].js",
-    publicPath: "/static/react/dist/"
   },
-  plugins: [new ExtractCSS("styles.css")],
+  plugins: [new MiniCssExtractPlugin ("styles.css")],
 };
 
 module.exports = config;
